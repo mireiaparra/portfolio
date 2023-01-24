962be57235372d5928ae608b638d826a1f6a5493
Mireia Maldonado Parra <mirmalparra@gmail.com>
2023-01-24 12:35:51 +0100

    Fix font size

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import techImg from "../../data/techImg";
+import cvES from "../../files/CV_Mireia_MaldonadoES.pdf";
 
 export function About(){
     const eachTech = techImg.map((tech) => {
@@ -18,8 +19,8 @@ export function About(){
         <p className="projects__text">Sevillana y graduada en Comunicación Audiovisual. He probado suerte como guionista, doctoranda y bibliotecaria. Ahora he decidido reinventarme y alejarme de páginas de papel para plasmar mi creatividad en un nuevo formato: el de las páginas web.</p>
         </div>
         <div className="about__link--container">
-        <Link className="about__link" to="/portfolio/src/files/CV_Mireia_MaldonadoES.pdf" target="_blank">Ver CV</Link>
-        <Link className="about__link"to="/portfolio/src/files/CV_Mireia_MaldonadoES.pdf" target="_blank" download="CV_Mireia_Maldonado">Descargar CV</Link>
+        <Link className="about__link" to={cvES} target="_blank">Ver CV</Link>
+        <Link className="about__link"to={cvES} target="_blank" download="CV_Mireia_Maldonado">Descargar CV</Link>
         </div>
         <ul className="about__tech">{eachTech}
         </ul>
